fd1bbb00977af0117851573e8e7bc73dd4ff6d91
Kapil Jugnu <kr_kapil@hotmail.com>
2019-03-28 12:25:56 +0530

    updated middleware

--- middleware/verifyAuth.js
+++ middleware/verifyAuth.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
     if (req.cookies.token !== 'c72cf1b0-5398-46de-96ad-86887409fdc8') {
-        res.sendStatus(401);
+        res.redirect('/login');
+        // res.sendStatus(401);
     }
     next()
 }
